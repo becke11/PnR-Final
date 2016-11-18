145c090999221e84bfbaf1a7a1e0960a34125cc2
becke11 <becke@gilmour.org>
2016-11-18 09:33:26 -0500

    int the set_left_speed and set_right_speed outputs

--- student.py
+++ student.py
@@ -133,8 +133,8 @@ class GoPiggy(pigo.Pigo):
     slef.stop()
 
     def setSpeed(self, left, right):
-        set_left_speed(left)
-        set_right_speed(right)
+        set_left_speed(int(left))
+        set_right_speed(int(right))
         time.sleep(.05)
 
 
